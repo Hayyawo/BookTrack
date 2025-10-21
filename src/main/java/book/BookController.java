02dcf773bed56f01d02671eaf42cd75a349dfe2a
Jan Czapiewski (ext) <jan.czapiewski.external@eviden.com>
2025-10-21 13:05:02 +0200

    added BookController

---
+++ src/main/java/book/BookController.java
@@ -0,0 +1,33 @@
+package book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/available")
+    public Page<Book> getAvailableBooks(
+            @PageableDefault(size = 20, sort = "title") Pageable pageable) {
+        return bookService.getAvailableBooks(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+}
