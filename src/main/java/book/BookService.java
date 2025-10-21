ca4d34d097e5c7f23aef4a09d622f0bdf0141281
Jan Czapiewski (ext) <jan.czapiewski.external@eviden.com>
2025-10-21 12:50:28 +0200

    added BookService

---
+++ src/main/java/book/BookService.java
@@ -0,0 +1,29 @@
+package book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public Page<Book> getAvailableBooks(Pageable pageable) {
+        return bookRepository.findByAvailableTrue(pageable);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    @Transactional
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
