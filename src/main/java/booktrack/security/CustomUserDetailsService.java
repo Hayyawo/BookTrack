b2e42d07e80edb85a6605c83d3244b64d96063c1
Jan Czapiewski (ext) <jasiuczapiewski99@gmail.com>
2025-10-24 10:08:13 +0200

    added CustomUserDetailsService which will be used to fetch data about user

--- src/main/java/booktrack/security/CustomUserDetailsService.java
+++ src/main/java/booktrack/security/CustomUserDetailsService.java
@@ -1,4 +1,19 @@
 package booktrack.security;
 
-public class CustomUserDetailsService {
+import booktrack.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email " + email));
+    }
 }
