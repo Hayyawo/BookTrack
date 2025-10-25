6999a6e81b987cccc458b168bc694f02267db0c3
Jan Czapiewski (ext) <jasiuczapiewski99@gmail.com>
2025-10-25 14:42:42 +0200

    added dtos for authentication

--- src/main/java/booktrack/security/dto/AuthenticationResponse.java
+++ src/main/java/booktrack/security/dto/AuthenticationResponse.java
@@ -1,4 +1,16 @@
 package booktrack.security.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class AuthenticationResponse {
+    private String token;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role;
 }
--- src/main/java/booktrack/security/dto/RegisterRequest.java
+++ src/main/java/booktrack/security/dto/RegisterRequest.java
@@ -1,4 +1,31 @@
 package booktrack.security.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+            message = "Password must contain digit, lowercase, uppercase, and special character"
+    )
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 100)
+    private String lastName;
 }
