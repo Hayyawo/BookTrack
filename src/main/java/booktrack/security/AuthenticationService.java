7ed595ccbf9bbc4340ae6b4fa15b2e47fe7f52a2
Jan Czapiewski (ext) <jasiuczapiewski99@gmail.com>
2025-10-26 20:02:59 +0100

    added AuthenticationService and register and authenticate method, changed package private to public to be able to use

---
+++ src/main/java/booktrack/security/AuthenticationService.java
@@ -0,0 +1,66 @@
+package booktrack.security;
+
+import booktrack.exceptions.ResourceNotFoundException;
+import booktrack.security.dto.AuthenticationRequest;
+import booktrack.security.dto.AuthenticationResponse;
+import booktrack.security.dto.RegisterRequest;
+import booktrack.user.Role;
+import booktrack.user.User;
+import booktrack.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = User.builder()
+                .email(registerRequest.getEmail())
+                .password(registerRequest.getPassword())
+                .firstName(registerRequest.getFirstName())
+                .lastName(registerRequest.getLastName())
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        String jwt = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwt)
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .role(user.getRole().toString())
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()));
+
+        User user = userRepository.findByEmail(authenticationRequest.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        String jwt = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwt)
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .role(user.getRole().toString())
+                .build();
+    }
+}
