0555b4895ae9b2a0adda44ba994557edb1534947
Jan Czapiewski (ext) <jasiuczapiewski99@gmail.com>
2025-10-30 10:29:02 +0100

    added two methods to controller

--- src/main/java/booktrack/loan/LoanController.java
+++ src/main/java/booktrack/loan/LoanController.java
@@ -4,6 +4,9 @@ import booktrack.loan.dto.CreateLoanRequest;
 import booktrack.loan.dto.LoanDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +23,19 @@ public class LoanController {
     }
 
     @PutMapping("/{loanId}/return")
-    @ResponseStatus(HttpStatus.ACCEPTED)
     public LoanDto returnBook(@PathVariable Long loanId) {
         return loanService.returnBook(loanId);
     }
+
+    @GetMapping("/user/{userId}")
+    public Page<LoanDto> getUserLoans(@PathVariable Long userId,
+                                      @PageableDefault(size = 20, sort = "loanDate") Pageable pageable) {
+        return loanService.getUserLoans(userId, pageable);
+    }
+
+    @GetMapping("/{id}")
+    public LoanDto getLoan(@PathVariable Long id) {
+        return loanService.getLoanById(id);
+    }
+
 }
