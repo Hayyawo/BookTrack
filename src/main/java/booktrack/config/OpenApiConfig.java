a2e07ceb801c59081e68f66d0a4fb46a7e3c4d99
Jan Czapiewski (ext) <jasiuczapiewski99@gmail.com>
2025-11-06 11:56:25 +0100

    added OpenApiConfig

---
+++ src/main/java/booktrack/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package booktrack.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "BookTrack API",
+                description = "Book lending management system",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Your Name",
+                        email = "your.email@example.com"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
--- src/main/java/booktrack/config/SecurityConfig.java
+++ src/main/java/booktrack/config/SecurityConfig.java
@@ -1,5 +1,7 @@
-package booktrack.security;
+package booktrack.config;
 
+import booktrack.security.CustomUserDetailsService;
+import booktrack.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
