f53c32080218535a1d5e5c0c451156741056a2cb
Jan Czapiewski (ext) <jasiuczapiewski99@gmail.com>
2025-11-13 11:43:56 +0100

    expanded caching

--- src/main/java/booktrack/config/CacheConfig.java
+++ src/main/java/booktrack/config/CacheConfig.java
@@ -4,8 +4,8 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,10 +15,19 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("books", "availableBooks");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(
+                "books",
+                "availableBooks",
+                "users",
+                "loans",
+                "userLoans",
+                "overdueLoans"
+        );
+
         cacheManager.setCaffeine(Caffeine.newBuilder()
                 .expireAfterWrite(10, TimeUnit.MINUTES)
-                .maximumSize(100));
+                .maximumSize(500));
+
         return cacheManager;
     }
 }
